9b5ee6bb1d0aadf0692c553e3bdc4dd459b9af38
hheiling <hheiling@live.unc.edu>
2019-02-05 21:18:46 -0500

    Upload of example files and updated README file with instructions and helpful information.

---
+++ Stack Exchange Example.R
@@ -0,0 +1,51 @@
+## Example for Stack Exchange
+
+# library(devtools)
+# library(lme4)
+# library(irr)
+# library(ncvreg)
+# library(mvtnorm)
+# library(ROCR)
+# library(Rcpp)
+# library(RcppArmadillo)
+# library(inline)
+# library(grpreg)
+# library(bigmemory)
+# library(bigalgebra)
+# library(BH)
+# library(glmnet)
+
+library(HelpWithZnewgen2)
+
+set.seed(1)
+U = matrix(rnorm(3000), nrow=100, ncol=30)
+Z = matrix(rnorm(15000), nrow = 500, ncol = 30)
+group = rep(1:10, each = 50)
+cols = c(1,11,21)
+n = 500
+q = 3
+d = 10 
+Znew = big.matrix(nrow = nrow(Z)*nrow(U), ncol = 6)
+J_SpMat = Matrix(0, 9, 6, sparse = TRUE)
+  sumy = 0
+  sumx = 0
+  zeros = 0
+  for(i in 1:3){
+    J_SpMat[ sumx + zeros + 1:(3 - (i-1)), sumy + 1:(3 - (i-1))] = diag((3 - (i-1)))
+    sumy = sumy + (3 - (i-1))
+    sumx = sumy
+    zeros = zeros + i
+  }
+
+## Running example by sourcing the .cpp file by itself (no error): (uncomment line below)
+Znew_gen2(U, Z, group, cols, n, q, d, Znew@address, J_SpMat)
+    ## This ran properly if the lines "First For Loop" and "Second For Loop" appear
+
+## Running example using the compiled R package:
+Znew_gen2.Rfunction(U, Z, group, cols, n, q, d, Znew@address, J_SpMat)
+
+## Error received:
+# error: arma::memory::acquire(): out of memory
+# Error in Znew_gen2(U, Z, group, cols, n, q, d, pBigMat, J) : 
+#   std::bad_alloc
+
---
+++ Upload Instructions.R
@@ -0,0 +1,26 @@
+# Upload instructions:
+
+# Link to GitHub: https://github.com/hheiling/myrepo_R
+## This Github repo includes: the package 'HelpWithZnewgen2' (all of its files) and 
+## the .tar.gz version of the package
+
+library(devtools)
+library(remotes)
+install_github("hheiling/myrepo_R", subdir = "HelpWithZnewgen2")
+
+# Needed libraries:
+
+# library(devtools)
+# library(lme4)
+# library(irr)
+# library(ncvreg)
+# library(mvtnorm)
+# library(ROCR)
+# library(Rcpp)
+# library(RcppArmadillo)
+# library(inline)
+# library(grpreg)
+# library(bigmemory)
+# library(bigalgebra)
+# library(BH)
+# library(glmnet)
