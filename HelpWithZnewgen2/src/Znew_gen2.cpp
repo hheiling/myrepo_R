bef674aeaffd38ee00f004a22c715c296a90d067
hheiling <hheiling@live.unc.edu>
2019-02-05 20:49:47 -0500

    Upload of files and .tar.gz version of "HelpWithZnewgen2" package. Version 1.0

---
+++ HelpWithZnewgen2/src/Znew_gen2.cpp
@@ -0,0 +1,59 @@
+
+#define ARMA_64BIT_WORD
+#include <RcppArmadillo.h>
+// [[Rcpp::depends(BH, bigmemory, RcppArmadillo)]]
+#include <bigmemory/MatrixAccessor.hpp>
+#include <numeric>
+
+using namespace Rcpp ;
+
+// [[Rcpp::export]]
+void Znew_gen2( const arma::mat& U, const arma::mat& Z, const arma::vec& g, const arma::vec& cols, 
+                unsigned int n, unsigned int q, unsigned int d, SEXP pBigMat, 
+                arma::sp_mat& J){ 
+    // Rcpp::XPtr<BigMatrix> pBigMat
+    // SEXP pBigMat (then added line below)
+  
+  unsigned int i = 0;
+  unsigned int j = 0;
+  unsigned int nMC = U.n_rows;
+  unsigned int index = 0;
+  int gr = 0;
+  arma::mat Usub(1, q);
+  arma::mat Zsub(1, q);
+  arma::mat out(n*nMC, J.n_cols);
+  XPtr<BigMatrix> pMat(pBigMat); // Added when using SEXP
+  MatrixAccessor<double> out2(*pMat); //Exchanged for line below when using SEXP
+  //MatrixAccessor<double> out2(*pBigMat); // Original
+  
+  Rprintf("First For Loop \n");
+  for(i = 0; i<n; i++){
+    // Rprintf("outer %u \n", i);
+    gr = g(i);
+    // Rprintf("g(i): %u \n", g(i));
+    // Rprintf("g(2): %u \n", g(2));
+    //Rprintf("gr: %u \n", gr);
+    for(index = 0;index<q; index++){
+      // Rprintf("inner 'index' first %u \n", index);
+      // Rprintf("cols(index)-1 + gr - 1: %u \n", cols(index)-1 + gr - 1);
+      Zsub(0,index) = Z(i,cols(index)-1 + gr - 1);
+    }
+    for(j = 0; j < nMC; j++){
+      // Rprintf("inner 'j' %u \n", j);
+      for(index = 0;index<q; index++){
+        // Rprintf("inner 'index' second %u \n", index);
+        Usub(0,index) = U(j,cols(index)-1 + gr - 1);
+      }
+      out.row((i*nMC + j)) = kron(Usub, Zsub) * J;
+    }
+  }
+  
+  Rprintf("Second For Loop \n");
+  for(i = 0; i<n*nMC; i++){
+    for(j = 0; j<out.n_cols; j++){
+      out2[j][i] = out(i, j);
+    }
+  }
+  //return wrap(out);
+}
+
